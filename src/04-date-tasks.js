9e8d5e4abb77efff7af78127ac9b22eaf76de54a
Пользователь <ksu1ven@mail.ru>
2023-06-06 22:55:48 +0300

    feat: do date tasks and almost all object tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return Date.parse(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return Date.parse(value);
 }
 
 
@@ -53,8 +53,10 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  if (date.getFullYear() % 100 === 0 && date.getFullYear() % 400 === 0) return true;
+  if (date.getFullYear() % 100 === 0 && date.getFullYear() % 400 !== 0) return false;
+  return date.getFullYear() % 4 === 0;
 }
 
 
@@ -73,8 +75,9 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const str = JSON.stringify(new Date(endDate - startDate));
+  return str.slice(str.indexOf('T') + 1, str.length - 2);
 }
 
 
@@ -94,8 +97,18 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const π = Math.PI;
+  let hours = date.getUTCHours();
+  if (hours > 12) hours -= 12;
+  const minutes = date.getUTCMinutes();
+  if (minutes === 0) {
+    if (hours === 0) return 0;
+    if (hours <= 6) return (π / 6) * hours;
+    if (hours > 6) return (π / 6) * (12 - hours);
+  }
+  const result = Math.abs((π / 6) * ((minutes / 5) - hours - (minutes / 60))).toFixed(16);
+  return result;
 }
 
 
